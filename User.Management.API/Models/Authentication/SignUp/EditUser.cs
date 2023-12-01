5973434137bd679647c0d1e61bd24d82933c803d
raju <lingaraju0411@gmail.com>
2023-12-01 16:08:43 +0530

    Simple

--- User.Management.API/Models/Authentication/SignUp/EditUser.cs
+++ User.Management.API/Models/Authentication/SignUp/EditUser.cs
@@ -5,8 +5,10 @@ namespace User.Management.API.Models.Authentication.SignUp
     public class EditUser
     {
         public string? Username { get; set; }
-                
+
         public string? Email { get; set; }
         public string? Role { get; set; }
+
     }
 }
+//aiuhclnsdKLCns;iocj
